6647974698b82aeeef057e6f4df147b3569bf2b8
TARANAMURADLI <tarana.muradli@gmail.com>
2023-03-26 21:37:45 +0400

    26032023

--- src/Header/İkinci/Əmanətlər.jsx
+++ src/Header/İkinci/Əmanətlər.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 
 const Əmanətlər = () => {
   return (
-    <div className='' >
-   <p>ddddasasasas</p>
-    </div>
+     <div className='onlaynnovbe'>
+    <div className='emanettotal'>
+      <h1 className='emanet1'>Əmanətlər</h1>
+    <div className='emanetsifaris'>
+      <h3>Universal Əmanət</h3>
+      <p>"Universal əmanət ilə əmanət şərtlərini sərbəst şəkildə seçin və əlavə gəlir,<br/>
+      qazanın!</p>
+      <div className='mud'>
+      <p className='mud1'>Müddət</p>
+      <h3 className='otuzaltiay'>36 ayadək</h3>
+      <p className='mud2'>Faiz dərəcəsi</p>
+      <h3 className='onelli'>10.50%</h3>
+      </div>
+    <div className='valyutaa'>
+      <p className='valy'>Valyuta</p>
+      <h3 className='aznusd'>AZN/EUR/USD</h3>  </div>
+      <div className='seyf1' ><img className='seyfsekil' src='https://www.rabitabank.com/resized/resize0x196/center/pages/138/285297343-588353235899703-7333212288748103418-n.png'/>
+      </div>
+     </div>
+     </div>
+     
+
+      
+        </div>
+      
+   
+
+   
   )
 }
 
